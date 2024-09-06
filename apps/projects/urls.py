b897f6977cac7d271ca9ac685ed6d069b9d61c4e
klad77 <7vlad@ukr.net>
2024-09-06 21:59:28 +0200

    lesson 1. task 8

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.projects.views.project_views import ProjectsListAPIView
+from apps.projects.views.project_views import ProjectsListAPIView, ProjectDetailAPIView
 
 
 urlpatterns = [
     path('', ProjectsListAPIView.as_view()),
+    path('<int:pk>/', ProjectDetailAPIView.as_view()),
 ]
