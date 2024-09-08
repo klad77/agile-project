6c5c3d741134dfcab3a1ae6346bc01bd59b64f04
klad77 <7vlad@ukr.net>
2024-09-08 16:16:48 +0200

    lesson 3. task 6

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -4,7 +4,8 @@ from apps.tasks.views.task_views import *
 
 
 urlpatterns = [
-    path('', TasksListAPIView.as_view()),
+    path('', TaskViewListCreateGenericView.as_view()),
+    # path('', TasksListAPIView.as_view()),
     path('<int:pk>/', TaskDetailAPIView.as_view()),
     path('tags/', TagListAPIView.as_view()),
     path('tags/<int:pk>/', TagDetailAPIView.as_view()),
